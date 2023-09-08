629e290e523687305a47b68628bdef00d8e80409
Saikumar <111180176+saikumar506@users.noreply.github.com>
2023-09-08 17:18:07 +0530

    Update app.py

--- app.py
+++ app.py
@@ -7,7 +7,6 @@ app = Flask(__name__)
 def welcome():
     return "Welcome to my python flak application"
 
-
 if __name__ == "__main__":
     port = int(os.environ.get("PORT", 6000))
     app.run(debug=True,host='0.0.0.0',port=port)
